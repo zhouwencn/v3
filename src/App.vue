4dc3ba8d0b9b94413d7eca63dc40acd83ee63416
jack <739856334zhou@gmail.com>
2025-12-18 12:17:01 +0800

    复制成功

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <script lang="ts" setup>
-// import { useGreyAndColorWeakness } from "@@/composables/useGreyAndColorWeakness"
-// import { usePany } from "@@/composables/usePany"
-// import { useTheme } from "@@/composables/useTheme"
+import { useGreyAndColorWeakness } from "@@/composables/useGreyAndColorWeakness"
+import { usePany } from "@@/composables/usePany"
+import { useTheme } from "@@/composables/useTheme"
 import zhCn from "element-plus/es/locale/lang/zh-cn" // Element Plus 中文包
 
-// const { initTheme } = useTheme()
+const { initTheme } = useTheme()
 
-// const { initGreyAndColorWeakness } = useGreyAndColorWeakness()
+const { initGreyAndColorWeakness } = useGreyAndColorWeakness()
 
-// const { initStarNotification, initStoreNotification, initMobileNotification } = usePany()
+const { initStarNotification, initStoreNotification, initMobileNotification } = usePany()
 
-// // 初始化主题
-// initTheme()
+// 初始化主题
+initTheme()
 
-// // 初始化灰色模式和色弱模式
-// initGreyAndColorWeakness()
+// 初始化灰色模式和色弱模式
+initGreyAndColorWeakness()
 
-// // #region 初始化通知
-// initStarNotification()
+// #region 初始化通知
+initStarNotification()
 
-// initStoreNotification()
+initStoreNotification()
 
-// initMobileNotification()
+initMobileNotification()
 // #endregion
 </script>
 
@@ -29,5 +29,4 @@ import zhCn from "element-plus/es/locale/lang/zh-cn" // Element Plus 中文包
   <el-config-provider :locale="zhCn">
     <router-view />
   </el-config-provider>
-  <!-- <div>123</div> -->
 </template>
